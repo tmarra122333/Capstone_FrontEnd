54ac565afdcba78274985bf8a3fec7ef184994dd
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-15 19:30:38 -0400

    pic load

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <img src="https://i.imgur.com/J8zXetn.jpg" alt="Boy in a jacket" width="600px" height="400px"></img>
-      <h2> Tom Marra is a Ny based producer, engineer, mixer, and musician for over 20 years.  Recently graduated from an Immersive Software Engineering Program at General Assembly and looks forward to creating Virtual Studio Plugins for all musicians to utilize.  </h2>
+      <img style={{ width:"900px", height:"600px", border:"solid #e03499 5px",}} img src="https://i.imgur.com/J8zXetn.jpg" alt="Boy"></img>
+      <h2> Tom Marra is a New York based producer, engineer, mixer, and musician for over 20 years.  Recently graduated from an Immersive Software Engineering Program at General Assembly and looks forward to creating Virtual Studio Plugins for all musicians to utilize.  </h2>
       </div>
   )
 }
