b551cc1116dead961351a89131dfa2f7a9ccd843
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-15 19:32:58 -0400

    finally complete i think

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 function Home(props) {
   return <div className="title" data-splitting="lines">
-<img src="https://mail.google.com/mail/u/0?ui=2&ik=9b848b3aa5&attid=0.1&permmsgid=msg-f:1727329680542616289&th=17f8b53152bb4ae1&view=att&disp=safe" alt="Boy in a jacket" width="1450px" height="775px"></img>
+<img src="https://i.imgur.com/2mC7nCB.jpg" alt=": (" width="1450px" height="775px"></img>
 </div>
 }
 export default Home;
